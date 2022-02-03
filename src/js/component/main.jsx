ae4d144e67b0013d9858e8608a787f4a953e55b3
PedroParraPerez <pedroparra06091994@gmail.com>
2022-02-03 10:56:39 +0000

    finish crono

--- src/js/component/main.jsx
+++ src/js/component/main.jsx
@@ -3,13 +3,9 @@ import "../../styles/main.css";
 
 const Main = () => {
 	const [seconds, setSeconds] = useState(0);
-	const [decseconds, setDecseconds] = useState(0);
-	const [minutes, setMinutes] = useState(0);
-	const [decminutes, setDecminutes] = useState(0);
 
 	useEffect(() => {
-		//Se ejecuta la función cada segundo y se suma uno a seconds
-		const interval = setInterval(() => {
+		setInterval(() => {
 			setSeconds((seconds) => seconds + 1);
 		}, 1000);
 	}, []);
@@ -21,18 +17,8 @@ const Main = () => {
 					<div className="icon">
 						<img src="https://img.icons8.com/material-outlined/120/000000/clock--v1.png" />
 					</div>
-					{/* Decenas de horas */}
-					<div className="time">{}</div>
-					{/* Horas */}
-					<div className="time">{}</div>
-					{/* Decenas de minutos */}
-					<div className="time">{Math.floor(seconds / 600)}</div>
-					{/* Minutos */}
-					<div className="time">{Math.floor(seconds / 60)}</div>
-					{/* Decenas de segundos */}
-					<div className="time">{Math.floor(seconds / 10)}</div>
 					{/* Segundos */}
-					<div className="time">{Math.floor(seconds)}</div>
+					<div className="time">{seconds}</div>
 				</div>
 			</div>
 		</>
