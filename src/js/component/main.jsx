60f50dab1d6c3816c4c1c1f5d0cfa326c4867812
PedroParraPerez <pedroparra06091994@gmail.com>
2022-02-03 10:01:49 +0000

    update crono

--- src/js/component/main.jsx
+++ src/js/component/main.jsx
@@ -3,16 +3,16 @@ import "../../styles/main.css";
 
 const Main = () => {
 	const [seconds, setSeconds] = useState(0);
+	const [decseconds, setDecseconds] = useState(0);
+	const [minutes, setMinutes] = useState(0);
+	const [decminutes, setDecminutes] = useState(0);
 
-	// setInterval(() => {
-	// 	{
-	// 		console.log("hola");
-	// 	}
-	// }, 1000);
-
-	setInterval(() => {
-		setSeconds((time) => time + 1);
-	}, 1000);
+	useEffect(() => {
+		//Se ejecuta la función cada segundo y se suma uno a seconds
+		const interval = setInterval(() => {
+			setSeconds((seconds) => seconds + 1);
+		}, 1000);
+	}, []);
 
 	return (
 		<>
@@ -21,7 +21,18 @@ const Main = () => {
 					<div className="icon">
 						<img src="https://img.icons8.com/material-outlined/120/000000/clock--v1.png" />
 					</div>
-					<div className="secounds">{seconds}</div>
+					{/* Decenas de horas */}
+					<div className="time">{}</div>
+					{/* Horas */}
+					<div className="time">{}</div>
+					{/* Decenas de minutos */}
+					<div className="time">{Math.floor(seconds / 600)}</div>
+					{/* Minutos */}
+					<div className="time">{Math.floor(seconds / 60)}</div>
+					{/* Decenas de segundos */}
+					<div className="time">{Math.floor(seconds / 10)}</div>
+					{/* Segundos */}
+					<div className="time">{Math.floor(seconds)}</div>
 				</div>
 			</div>
 		</>
